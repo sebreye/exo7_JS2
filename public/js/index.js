9a712e7a41c70a63c9cb9a0fe8425b732d585f26
sebastian Reyes <sebreyes222gmail.com>
2023-01-04 12:04:18 +0100

    ajout class

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+class Personnage {
+    constructor(age, nom, ville) {
+        this.age = age;
+        this.nom = nom;
+        this.ville = ville;
+    }
+}
+let personnage1 = new Personnage(30, 'Alice', 'Paris');
+let personnage2 = new Personnage(20, 'Bob', 'Lyon')
