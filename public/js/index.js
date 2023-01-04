20c1396e705f0b6ea2de9f585c478eac7ceb6fa5
sebastian Reyes <sebreyes222gmail.com>
2023-01-04 12:07:45 +0100

    ajout sepresenter

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,10 @@ class Personnage {
         this.nom = nom;
         this.ville = ville;
     }
+
+    sePresenter(){
+        console.log(`Bonjour je m'appelle ${this.nom}`);
+    }
 }
 let personnage1 = new Personnage(30, 'Alice', 'Paris');
 let personnage2 = new Personnage(20, 'Bob', 'Lyon')
